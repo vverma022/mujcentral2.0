af2b1c9058761dca7f1d82c4c7217b11e1edbaaf
Vasu Verma <vermavasu069@gmail.com>
2025-01-02 19:57:45 +0530

    Fetching Confessions

--- components/content/confession-list.tsx
+++ components/content/confession-list.tsx
@@ -1,19 +1,7 @@
+"use client";
 import ConfessionPost from './confession-post'
 import MaxWidthWrapper from '../shared/max-width-wrapper'
 import { useState, useEffect } from 'react'
-
-const confessions = [
-  {
-    id: 1,
-    confession: "I secretly love pineapple on pizza.",
-    username: "PizzaLover123",
-  },
-  {
-    id: 2,
-    confession: "I've never seen Star Wars.",
-    username: "MovieBuff99",
-  },
-]
 
 export default function ConfessionList() {
   const [confessions, setConfessions] = useState([]);
@@ -23,7 +11,7 @@ export default function ConfessionList() {
   useEffect(() => {
     const fetchConfessions = async () => {
       try {
-        const response = await fetch('/api/confess/list');
+        const response = await fetch('/api/confess/fetch');
         if (!response.ok) {
           throw new Error('Failed to fetch confessions');
         }
@@ -41,19 +29,20 @@ export default function ConfessionList() {
     fetchConfessions();
   }, []);
 
+  if (loading) return <p className='text-center'>Loading confessions...</p>;
+  if (error) return <p className='text-center'>{error}</p>;
+
   return (
     <MaxWidthWrapper className="py-6 md:pb-8 md:pt-10">
-  <div className="max-w-screen-sm">
-    <h1 className="font-heading text-3xl md:text-4xl pb-2">
-      Confessions
-    </h1>
-  </div>
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6">
-    {confessions.map((confession) => (
-      <ConfessionPost key={confession,} {...confession} />
-    ))}
-  </div>
-</MaxWidthWrapper>
-  )
+      <div className="max-w-screen-sm">
+        <h1 className="font-heading text-3xl md:text-4xl pb-2">Confessions</h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6">
+        {confessions.map((confession: { username: string, confession: string, id: number }) => (
+          <ConfessionPost key={confession.id} username={confession.username} confession={confession.confession} />
+        ))}
+      </div>
+    </MaxWidthWrapper>
+  );
 }
 
