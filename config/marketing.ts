1f7eb3648b85063e581c5b9ab503b1301c2794e6
Vasu Verma <vermavasu069@gmail.com>
2025-01-24 02:30:03 +0530

    Removed unimates from navbar

--- config/marketing.ts
+++ config/marketing.ts
@@ -3,10 +3,6 @@ import { MarketingConfig } from "types"
 export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
-      title: "Unimates",
-      href: "/unimates",
-    },
-    {
       title: "Papers",
       href: "/#papers",
     },
