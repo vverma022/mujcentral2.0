6740dad2f0c0cb72c047f46ebf3925b14a52d06a
Vasu Verma <109036913+vverma022@users.noreply.github.com>
2025-01-24 03:36:30 +0530

    Update page.tsx

--- app/(features)/confess/page.tsx
+++ app/(features)/confess/page.tsx
@@ -3,7 +3,7 @@
 import { GetServerSideProps } from 'next';
 import ConfessionList from '@/components/confess/confession-list';
 
-const url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const url = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
 
 const fetchConfessions = async () => {
   const res = await fetch(`${url}/api/confess/fetch`); 
