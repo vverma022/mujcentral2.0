9797af6e19a1a45dfb3b95392cbdbe0f751ba9f0
Vasu Verma <vermavasu069@gmail.com>
2025-02-12 00:44:43 +0530

    Added AIML and WC papers for CCE

--- config/papers.ts
+++ config/papers.ts
@@ -221,6 +221,8 @@ export const thirdYearPapers: Record<string, Paper[]> = {
     {id: 1, title: 'AIML', name: "Artificial Intelligence and Machine Learning", link: 'https://drive.google.com/file/d/1eFXR7UiIDaBoBZtNErlzMVHamcFa_8U_/view?usp=share_link', image: R1, difficulty: 'M', examType: 'MTE'},
     {id: 2, title: 'ATCD', name: "Automata and Compiler Design", link: 'https://drive.google.com/file/d/1LW7ZeivpBDKjoyNotcVxQmbxfEVVvq_O/view?usp=share_link', image: R2, difficulty: 'M', examType: 'MTE'},
     {id: 3, title: 'ATCD', name: "Automata and Compiler Design", link: 'https://drive.google.com/file/d/1W0fSY1aqZ8pEfr1ByKwZYDAUSc34G6oO/view?usp=share_link', image: R3, difficulty: 'M', examType: 'MTE'},
+    {id: 4, title: 'AIML', name: "Artificial Intelligence and Machine Learning",link: "https://drive.google.com/file/d/1O3mWTyeHWGgDRPxa8z8vFT7QD2sYZZwj/view?usp=share_link", image: R1, difficulty: 'M', examType: 'ETE'},
+    {id: 5, title: 'WC', name: "Wireless Communication", link: 'https://drive.google.com/file/d/1f6OsjWC3YbkluDXtq7ID4VxpYVeEBS4t/view?usp=share_link', image: R2, difficulty: 'H', examType: 'ETE'},
   ],
   'IT': [
     {id: 1, title: 'CIS', name: "Cryptography", link: 'https://drive.google.com/file/d/13n2wtxgUo2636Z71-_1OxEOfH82guw5L/view?usp=share_link', image: R1, difficulty: 'M', examType: 'MTE'},
